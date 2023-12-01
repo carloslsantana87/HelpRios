b49ff5e9ade165a7ada2b7981c2578a2c4ea8a5a
carloslsantana87 <carloslsantana87@gmail.com>
2023-12-01 17:03:24 -0300

    Alteracaodestrutaura

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,4 +1,4 @@
-import { AppDataSource } from "../data-source"
+import { AppDataSource } from "../db/data-source"
 import { NextFunction, Request, Response } from "express"
 import { User } from "../entity/User"
 
